fe9fdca45a88cd0d38c3e7d8a6c30120aa20e142
Rogério de Almeida <roger.almeidaassessoria@gmail.com>
2020-12-20 13:15:10 -0300

    Conversão de moedas

---
+++ Dolar.py
@@ -0,0 +1,8 @@
+print('\n=========  R$   =========')
+val = float(input('Digite o valor da moéda: -> '))
+print('\n=========  U$   =========')
+dol = float(input('Digite o valor do dólar: -> '))
+conv = val / dol
+print('\n======= conversão =======')
+print('O valor da conversão é U$: {:.2f}'.format(conv))
+print('\n=========================')
