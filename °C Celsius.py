e0db876502f10ff38cfd8e5acd2fd5848f8c3d38
Rogério de Almeida <roger.almeidaassessoria@gmail.com>
2020-12-20 13:31:45 -0300

    Temperatura °C

---
+++ °C Celsius.py
@@ -0,0 +1,7 @@
+print('%'*32)
+print('========== TEMPERATURA =========')
+i = float(input('Digitte a temperatura °C: '))
+f = ((9*i)/5)+32
+print('temperatura de {} °C \ncoreespondente a {} °F! '.format(i, f))
+
+print('='*32)
