c4cc186deee823de5df0e3ef3ce36f9e10440595
Rogério de Almeida <roger.almeidaassessoria@gmail.com>
2020-12-20 13:22:01 -0300

    Achar a Raiz Quadrada

---
+++ raiz.py
@@ -0,0 +1,7 @@
+n1 = float  (input('Digite um Numero '))
+r = n1*2
+p = n1**(1/2)
+print('O dobro de: {}, é: {} '.format(n1, r))
+print('O tripo de {} é: {} '.format(n1, n1*3))
+print('A raiz quadrada de {} é: {:.2f} '.format(n1, p))
+
